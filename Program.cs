420b7b094a06d991f542e80d460776dbeaea03bf
oliver-enede <78216854+oliver-enede@users.noreply.github.com>
2024-06-03 16:07:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MoreArrays
+{
+    internal class Program
+    {
+        public static int SumArray(int[] arr)
+        {
+            int total = 0;
+            // Iterate through the array and add each element to total
+            foreach (int num in arr)
+            {
+                total += num;
+            }
+            return total;
+        }
+
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Sum of values in array: " + SumArray(arr));
+            Console.ReadLine();
+        }
+    }
+}
